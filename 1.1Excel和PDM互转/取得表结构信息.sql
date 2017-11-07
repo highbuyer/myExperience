dd8411dce1bc884f586fc70982193266654a3525
lyyMyGitHub <719882551@qq.com>
2017-11-07 14:30:21 +0800

    修改数据库导出脚本

--- 1.1Excel和PDM互转/取得表结构信息.sql
+++ 1.1Excel和PDM互转/取得表结构信息.sql
@@ -1,17 +1,47 @@
-SELECT
-	TABLE_NAME 表名称,
-	COLUMN_NAME 列名,
-	DATA_TYPE 字段类型,
-	COLUMN_TYPE 数据长度,
-	CHARACTER_MAXIMUM_LENGTH 长度,
-	IS_NULLABLE 是否为空,
-	COLUMN_DEFAULT 默认值,
-	COLUMN_COMMENT 备注
-FROM
-	INFORMATION_SCHEMA. COLUMNS
-WHERE
-	table_schema = 'zdtest';
-
--- AND
--- table_name  = 'article'
--- article为表名，到时候换成你要导出的表的名称
+select
+	'logistics' 主题域,
+  	tables.table_comment 表注释,
+	columns.table_name 表英文名称,
+	tables.table_comment 表中文名称,
+	columns.column_name 列英文名称,
+	columns.column_comment 列中文名称,
+	columns.column_comment 列注释,
+	columns.column_type 数据类型,
+	columns.column_key 是否主键,
+	columns.is_nullable 是否为空,
+	columns.column_default 默认值
+from
+	(
+		select
+			table_name,
+			table_comment
+		from
+			information_schema. tables
+		where
+			-- 此处修改数据库名称 
+			table_schema = 'xyzj'
+			-- 以下输入表名称
+			 AND	
+			 table_name  = 'user'
+	) tables,
+	(
+		select
+			table_name,
+			column_name,
+			column_type,
+			column_key,
+			character_maximum_length,
+			is_nullable,
+			column_default,
+			column_comment
+		from
+			information_schema. columns
+		where
+			-- 此处修改数据库名称 
+			table_schema = 'xyzj'
+			-- 以下输入表名称
+			 AND	
+			 table_name  = 'user'
+	) columns
+where
+	tables.table_name = columns.table_name;
